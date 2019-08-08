27445519d2a54abb3f8489e30315d72da2f8cbe4
kkassem <kkassem@kassem>
2019-08-08 09:54:09 +0300

    Add creation Daily_Attend table Script

--- db_Script/20190808CREATE_DAILYATTEND_TABLE.sql
+++ db_Script/20190808CREATE_DAILYATTEND_TABLE.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS daily_attend (
     id INT AUTO_INCREMENT,
-	userId VARCHAR(255) ,
+	user_Id VARCHAR(255) ,
 	start_Time VARCHAR(255) ,
 	user_name VARCHAR(255) ,
 	date date,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id) 
 )  ENGINE=INNODB;
